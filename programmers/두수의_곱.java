56a54ebc4e1ce59f0d8ddc100b31bb8b55d306bb
JoDaeUk <du39574@gmail.com>
2022-12-25 16:47:31 +0900

    두수의_곱

---
+++ programmers/두수의_곱.java
@@ -0,0 +1,20 @@
+package programmers;
+
+public class 두수의_곱 {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 4;
+        System.out.println(solution(num1, num2));
+
+        num1 = 27;
+        num2 = 19;
+        System.out.println(solution(num1, num2));
+    }
+
+        public static int solution(int num1, int num2) {
+            int answer = 0;
+            return answer = num1 * num2;
+
+    }
+}
+
