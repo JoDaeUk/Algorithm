205e9a3b9d89cecb40be5c6af120bf788ba4b52b
JoDaeUk <du39574@gmail.com>
2022-12-26 09:01:46 +0900

    몫_구하기

---
+++ programmers/몫_구하기.java
@@ -0,0 +1,19 @@
+package programmers;
+
+public class 몫_구하기 {
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 5;
+        System.out.println(solution(num1,num2));
+
+        num1 = 7;
+        num2 = 2;
+        System.out.println(solution(num1,num2));
+    }
+
+        public static int solution(int num1, int num2) {
+            return num1/num2;
+
+    }
+}
