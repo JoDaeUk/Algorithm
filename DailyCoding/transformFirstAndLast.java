dfd4a85e1463224475a10f9523d3ee21266d02f8
JoDaeUk <du39574@gmail.com>
2023-01-12 10:03:22 +0900

    transformFirstAndLast

---
+++ DailyCoding/transformFirstAndLast.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class transformFirstAndLast {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        String[] arr1 = new String[]{"Queen", "Elizabeth", "Of Hearts", "Beyonce"};
+        HashMap<String, String> output1 = transformFirstAndLast(arr1);
+        System.out.println(output1); // --> { Queen : 'Beyonce' }
+
+        String[] arr2 = new String[]{"Kevin", "Bacon", "Love", "Hart", "Costner", "Spacey"};
+        HashMap<String, String> output2 = transformFirstAndLast(arr2);
+        System.out.println(output2); // --> { Kevin : 'Spacey' }
+    }
+
+    public static HashMap<String, String> transformFirstAndLast(String[] arr) {
+        // TODO:
+        if (arr.length == 0)
+            return null;
+        HashMap<String, String> result = new HashMap<String, String>();
+        result.put(arr[0], arr[arr.length - 1]);
+        return result;
+    }
+}
