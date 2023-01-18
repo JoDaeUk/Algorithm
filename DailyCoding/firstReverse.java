4ec4f352262f4f54929f9cef26a9b81076c18e1f
JoDaeUk <du39574@gmail.com>
2023-01-18 09:38:55 +0900

    firstReverse

--- DailyCoding/firstReverse.java
+++ DailyCoding/firstReverse.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 
-public class  {
+public class  firstReverse{
     public static void main(String[] args) {
         String output = firstReverse("codestates");
         System.out.println(output); // "setatsedoc"
