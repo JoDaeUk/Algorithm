19d5f3ac0992faeaa52f5c676bc0c090229452c5
JoDaeUk <du39574@gmail.com>
2023-01-18 09:38:27 +0900

    firstReverse

---
+++ DailyCoding/firstReverse.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class  {
+    public static void main(String[] args) {
+        String output = firstReverse("codestates");
+        System.out.println(output); // "setatsedoc"
+
+        output = firstReverse("I love codestates");
+        System.out.println(output); // "setatsedoc evol I"
+    }
+
+    public static String firstReverse(String str) {
+        // TODO:
+        return new StringBuilder(str).reverse().toString();
+
+        //   StringBuilder sb = new StringBuilder(str);
+        //   return sb.reverse().toString();
+        // }
+    }
+}
+
