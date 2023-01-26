d53b491711388fe1c4afa67de5d722ae94b221e0
JoDaeUk <du39574@gmail.com>
2023-01-26 09:53:35 +0900

    ABCheck

---
+++ DailyCoding/ABCheck.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class ABCheck {
+    public static void main(String[] args) {
+        boolean output = ABCheck("lane Borrowed");
+        System.out.println(output); // --> true
+    }
+
+    public static boolean ABCheck(String str) {
+        // TODO:
+        String[] arr = str.toUpperCase().split("");
+        for (int i = 0; i<arr.length-5; i++){
+            if(arr[i].equals("A")){
+                if(arr[i+4].equals("B")) return true;
+            }
+            if(arr[i].equals("B")){
+                if(arr[i+4].equals("A") ) return true;
+            }
+        }
+        return false;
+    }
+}
+
